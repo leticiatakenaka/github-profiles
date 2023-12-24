6eeac93b0f4614055f33135ad6d0503d7d499cbe
Leticia Santos Takenaka <leticiatakenaka12@gmail.com>
2023-12-24 12:00:38 -0300

    add search view

---
+++ github-profiles/Controllers/SearchController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace github_profiles.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
