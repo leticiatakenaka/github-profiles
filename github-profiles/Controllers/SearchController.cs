5fe68a6a6be50e6d8aa4322c01ed6c6945ea5693
Leticia Santos Takenaka <leticiatakenaka12@gmail.com>
2023-12-24 13:13:06 -0300

    Add api request

--- github-profiles/Controllers/SearchController.cs
+++ github-profiles/Controllers/SearchController.cs
@@ -8,5 +8,23 @@ namespace github_profiles.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Results(string username)
+        {
+            using HttpClient httpClient = new();
+            string apiUrl = $"https://api.github.com/users/{username}";
+
+            httpClient.DefaultRequestHeaders.Add("User-Agent", "leticiatakenaka");
+
+            HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                ViewData["profile"] = content;
+            }
+            return View("Index");
+        }
     }
 }
